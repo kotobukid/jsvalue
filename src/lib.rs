2fb5a03b54e2c9b6b7940a5aeb5fa5914cccecdc
kotobukid <kotobuki@daqua.jp>
2023-08-01 00:16:42 +0900

    りフォーマット

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 mod utils;
+
 use serde::{Serialize, Deserialize};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsValue;
@@ -7,12 +8,12 @@ use serde_wasm_bindgen::to_value;
 #[derive(Serialize, Deserialize)]
 struct Person {
     age: u32,
-    name: String
+    name: String,
 }
 
 #[derive(Serialize, Deserialize)]
 struct People {
-    people: Vec<Person>
+    people: Vec<Person>,
 }
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -33,21 +34,21 @@ pub fn greet(name: &str) {
 
 #[wasm_bindgen]
 pub fn flatten() -> JsValue {
-    return  JsValue::from_str("aaaaa")
+    return JsValue::from_str("aaaaa");
 }
 
 #[wasm_bindgen]
 pub fn get_person() -> JsValue {
-    let p = Person{age: 20, name: "Taro".into()};
-     to_value(&p).unwrap()
+    let p = Person { age: 20, name: "Taro".into() };
+    to_value(&p).unwrap()
 }
 
 #[wasm_bindgen]
 pub fn get_people() -> JsValue {
-    let p = Person{age: 20, name: "Taro".into()};
-    let p1 = Person{age: 21, name: "Jiro".into()};
-    let p2 = Person{age: 22, name: "Alice".into()};
-    let p3 = Person{age: 23, name: "Mercurius".into()};
+    let p = Person { age: 20, name: "Taro".into() };
+    let p1 = Person { age: 21, name: "Jiro".into() };
+    let p2 = Person { age: 22, name: "Alice".into() };
+    let p3 = Person { age: 23, name: "Mercurius".into() };
     let people = People {
         people: vec![p, p1, p2, p3]
     };
